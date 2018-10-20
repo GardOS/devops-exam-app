aa37893058684c2fe07c242c4ba2381762e80f26
GardOS <Gardo1506@gmail.com>
2018-10-20 11:43:04 +0000

    Add base for app

---
+++ src/main/kotlin/no/gardos/Hello.kt
@@ -0,0 +1,6 @@
+package no.gardos
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/no/gardos/HelloTest.kt
@@ -0,0 +1,8 @@
+package no.gardos
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}

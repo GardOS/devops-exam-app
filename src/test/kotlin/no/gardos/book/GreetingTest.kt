12f629e591d6b3addfbb51d1b3443fba7d222544
GardOS <gardo1506@gmail.com>
2018-11-21 18:04:32 +0100

    Add tests for greeting

---
+++ src/test/kotlin/no/gardos/book/GreetingTest.kt
@@ -0,0 +1,45 @@
+package no.gardos.book
+
+import io.restassured.RestAssured
+import io.restassured.RestAssured.get
+import junit.framework.TestCase.assertNotNull
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.context.embedded.LocalServerPort
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = [(BookApplication::class)])
+class GreetingTest {
+
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun initClass() {
+            RestAssured.baseURI = "http://localhost/"
+            RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+        }
+    }
+
+    @Before
+    fun init() {
+        RestAssured.port = port
+    }
+
+    @LocalServerPort
+    protected var port = 0
+
+    @Test
+    fun greeting_ok() {
+        get().then().statusCode(200)
+    }
+
+    @Test
+    fun greeting_hasContent() {
+        val greeting = get().body().print()
+        assertNotNull(greeting)
+    }
+}
